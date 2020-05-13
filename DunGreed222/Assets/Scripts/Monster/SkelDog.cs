2fedf88d358d049f59c444fe9b66a5e3b9f425ea
jongjin1597 <52647256+jongjin1597@users.noreply.github.com>
2020-05-13 14:42:37 +0900

    몹 프리팹 애니메이터폴더정리

---
+++ DunGreed222/Assets/Scripts/Monster/SkelDog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkelDog : MonoBehaviour
+{
+    Rigidbody2D _rigid;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(_rigid.position, Vector3.down, new Color(0, 1, 0));
+
+        if (_rigid.velocity.y < 0)
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(_rigid.position, Vector3.down * 2.5f, 1, LayerMask.GetMask("floor"));
+        }
+    }
+}
