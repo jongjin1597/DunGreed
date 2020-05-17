8b51d72820cf37c4785d406c88b4a2077ac0d52e
koodonghyeon <64832137+koodonghyeon@users.noreply.github.com>
2020-05-17 17:54:30 +0900

    강아지 완전수정

--- DunGreed222/Assets/Scripts/Monster/SkelDog.cs
+++ DunGreed222/Assets/Scripts/Monster/SkelDog.cs
@@ -100,5 +100,13 @@ public class SkelDog : cShortMonster
             }
             }
     }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
 
+        //피격판정및 공격판정 스타트용
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Attack();
+        }
+    }
 }
--- DunGreed222/Assets/Scripts/Monster/cDogBox.cs
+++ DunGreed222/Assets/Scripts/Monster/cDogBox.cs
@@ -5,14 +5,6 @@ using UnityEngine;
 public class cDogBox : MonoBehaviour
 {
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
- 
-        //피격판정및 공격판정 스타트용
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            transform.parent.GetComponent<SkelDog>().Attack();
-        }
-    }
+   
 }
 
